3f065eef8782831b9e0904c7e399ffd796a7f725
mgswi <mgswi@OCEAN>
2025-06-05 00:26:14 -0500

    Add project files.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/* This code below will print the words Hello World!
+	to the screen, and it is amazing */
+	cout << "Hello World!" << "\n\n";
+	cout << "I am learning c++" << "\n\n";
+
+	int Mynum = 5; //Integer (Whole number without decimals
+	return 0;
+}
